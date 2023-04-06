a1d44105a13a1a9a13b12307b3097f56ed50c8b8
dylan <dylan@qsx.onaliyun.com>
2023-04-06 17:23:08 +0800

    固定文本修改

--- src/spell.ts
+++ src/spell.ts
@@ -1,8 +1,20 @@
-import { FixSpell } from './spell-config.js'
+import { FixSpell, FixSpellRoom } from './spell-config.js'
 
 export const fixReply = (rawText: string, botName: string) => {
   let rtnText = ''
   for (let fix of FixSpell) {
+    if (fix.reg) {
+      if (fix.triger.test(rawText)) {
+        rtnText = fix.spell.replaceAll('牛牛', botName)
+      }
+    }
+  }
+  return rtnText
+}
+
+export const fixRoomReply = (rawText: string, botName: string, talkerName: string | boolean) => {
+  let rtnText = ''
+  for (let fix of FixSpellRoom) {
     if (fix.reg) {
       if (fix.triger.test(rawText)) {
         rtnText = fix.spell.replaceAll('牛牛', botName)
@@ -12,6 +24,7 @@ export const fixReply = (rawText: string, botName: string) => {
       if (fix.preBotName) triger = '@' + botName + triger
       if (rawText === triger) {
         rtnText = fix.spell.replaceAll('牛牛', botName)
+        if (!fix.noAite) rtnText = `@${talkerName} ${rtnText}`
       }
     }
   }
